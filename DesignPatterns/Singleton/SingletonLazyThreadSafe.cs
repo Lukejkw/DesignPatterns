40920022e141d8a85e049a761998e14112c3e634
Luke Warren <lukejkw@gmail.com>
2016-06-30 14:42:51 +0200

    name fix and cleanup

--- DesignPatterns/Singleton/SingletonLazyThreadSafe.cs
+++ DesignPatterns/Singleton/SingletonLazyThreadSafe.cs
@@ -1,11 +1,9 @@
-﻿using System;
-
-namespace DesignPatterns.Singleton
+﻿namespace DesignPatterns.Singleton
 {
     public sealed class SingletonLazyThreadSafe
     {
-        private static volatile SingletonLazyThreadSafe instance;
-        private static object _mutex = new Object();
+        private static volatile SingletonLazyThreadSafe _instance;
+        private static object _mutex = new object();
 
         private SingletonLazyThreadSafe()
         {
@@ -16,7 +14,7 @@ namespace DesignPatterns.Singleton
             get
             {
                 // First check if instance is null
-                if (instance == null)
+                if (_instance == null)
                 {
                     // Two threads could have checked the previous check at the same time
                     // so make sure that we only let 1 thread to use this block at a time
@@ -24,11 +22,11 @@ namespace DesignPatterns.Singleton
                     {
                         // Check again if the object is null - remember that if there were 2 threads the first would initialize the instance
                         // The second thread would still go into this block when it was his turn
-                        if (instance == null)
-                            instance = new SingletonLazyThreadSafe();
+                        if (_instance == null)
+                            _instance = new SingletonLazyThreadSafe();
                     }
                 }
-                return instance;
+                return _instance;
             }
         }
     }
